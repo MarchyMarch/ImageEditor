0bcb2b7e97a07492b4dcd2d648da52d78816ea66
kyle <kyle@marchymarch.byu.edu>
2017-01-10 12:51:44 -0700

    First Test Commit

---
+++ ImageEditor/src/ImageEditor.java
@@ -0,0 +1,12 @@
+
+public class ImageEditor
+{
+	public static void main(String[] args)
+	{
+		if(args.length > 3) System.out.println("Invalid args length");
+		
+		String inputFileName = args[0];
+		String outputFileName = args[1];
+		String transformation = args[2];
+	}
+}
